f682e1ba74a45e2107077cf3d350f8d25dca6a77
CodarZ <1476712061@qq.com>
2025-07-16 16:14:50 +0800

    feat: view

---
+++ src/views/system/user/index.tsx
@@ -0,0 +1,9 @@
+export default function SystemUserIndex() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>系统用户管理</h2>
+      <p>这是系统用户管理页面</p>
+      <p>路径：/company/user</p>
+    </div>
+  )
+}
