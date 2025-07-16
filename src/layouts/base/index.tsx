3685ddc2e45335f730c240e61e595a9e780cd52b
CodarZ <1476712061@qq.com>
2025-07-16 10:46:31 +0800

    feat: 渲染静态路由

--- src/layouts/base/index.tsx
+++ src/layouts/base/index.tsx
@@ -1,123 +1,96 @@
-import { LogoutOutlined, QuestionCircleFilled } from '@ant-design/icons'
-import { PageContainer, ProCard, ProLayout } from '@ant-design/pro-components'
-import { Dropdown } from 'antd'
-import { useState } from 'react'
-import defaultProps from './_defaultProps'
+import { ProLayout } from '@ant-design/pro-components'
+import { Outlet, useLocation, useNavigate } from 'react-router'
+import { mainRoute } from '@/routes/constants'
+import type { RouteObject } from 'react-router'
+
+interface MenuItem {
+  path: string
+  name: string
+  children?: MenuItem[]
+}
+
+// 将路由配置转换为 ProLayout 菜单格式
+function routeToMenu(routes: RouteObject[]): MenuItem[] {
+  return routes
+    .map((route) => {
+      // 处理索引路由
+      if (route.index) {
+        return {
+          path: '/',
+          name: '首页',
+        }
+      }
+
+      const menu: MenuItem = {
+        path: route.path || '',
+        name: getMenuName(route.path || ''),
+      }
+
+      if (route.children) {
+        const childMenus = routeToMenu(route.children.filter((child: RouteObject) => child.path && child.path !== '*'))
+        if (childMenus.length > 0) {
+          // 为子菜单添加完整路径
+          menu.children = childMenus.map((child) => ({
+            ...child,
+            path: `${menu.path}/${child.path}`.replace('//', '/'),
+          }))
+        }
+      }
+
+      return menu
+    })
+    .filter((item) => item.path && item.path !== '*')
+}
+
+// 根据路径生成菜单名称
+function getMenuName(path: string): string {
+  if (path === '/') return '首页'
+  if (path === '/error') return '错误页面'
+  if (path === '404') return '404页面'
+  if (path === '403') return '403页面'
+  return path.replace('/', '')
+}
 
 export default function BaseLayout() {
-  const [pathname, setPathname] = useState('/list/sub-page/sub-sub-page1')
+  const location = useLocation()
+  const navigate = useNavigate()
+
   if (typeof document === 'undefined') {
     return <div />
   }
+
+  // 从路由配置生成菜单数据
+  const menuData = mainRoute.children ? routeToMenu(mainRoute.children) : []
+
   return (
-    <ProLayout
-      layout="mix"
-      style={{ height: '100vh' }}
-      bgLayoutImgList={[
-        {
-          src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
-          left: 85,
-          bottom: 100,
-          height: '303px',
-        },
-        {
-          src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
-          bottom: -68,
-          right: -45,
-          height: '303px',
-        },
-        {
-          src: 'https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png',
-          bottom: 0,
-          left: 0,
-          width: '331px',
-        },
-      ]}
-      {...defaultProps}
-      location={{
-        pathname,
-      }}
-      token={{
-        header: {
-          colorBgMenuItemSelected: 'rgba(0,0,0,0.04)',
-        },
-      }}
-      avatarProps={{
-        src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-        size: 'small',
-        title: '七妮妮',
-        render: (props, dom) => {
-          return (
-            <Dropdown
-              menu={{
-                items: [
-                  {
-                    key: 'logout',
-                    icon: <LogoutOutlined />,
-                    label: '退出登录',
-                  },
-                ],
-              }}
-            >
-              {dom}
-            </Dropdown>
-          )
-        },
-      }}
-      actionsRender={(props) => {
-        if (props.isMobile) return []
-        if (typeof window === 'undefined') return []
-        return [<QuestionCircleFilled key="QuestionCircleFilled" />]
-      }}
-      headerTitleRender={(logo, title, _) => {
-        const defaultDom = (
-          <a>
-            {logo}
-            {title}
-          </a>
-        )
-        if (typeof window === 'undefined') return defaultDom
-        if (document.body.clientWidth < 1400) {
-          return defaultDom
-        }
-        if (_.isMobile) return defaultDom
-        return <>{defaultDom}</>
-      }}
-      menuFooterRender={(props) => {
-        if (props?.collapsed) return undefined
-        return (
+    <div style={{ height: '100vh' }}>
+      <ProLayout
+        layout="mix"
+        avatarProps={{
+          src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+          title: '超级管理员',
+        }}
+        menu={{
+          request: async () => {
+            return menuData
+          },
+        }}
+        location={{
+          pathname: location.pathname,
+        }}
+        menuItemRender={(item, dom) => (
           <div
-            style={{
-              textAlign: 'center',
-              paddingBlockStart: 12,
+            onClick={() => {
+              navigate(item.path || '/')
             }}
+            style={{ cursor: 'pointer' }}
           >
-            <div>©2025 React Web Client </div>
-            <div>by Ant Design</div>
+            {dom}
           </div>
-        )
-      }}
-      onMenuHeaderClick={(e) => console.log(e)}
-      menuItemRender={(item, dom) => (
-        <div
-          onClick={() => {
-            setPathname(item.path || '/welcome')
-          }}
-        >
-          {dom}
-        </div>
-      )}
-    >
-      <PageContainer title={false}>
-        <ProCard
-          style={{
-            height: '200vh',
-            minHeight: 800,
-          }}
-        >
-          <div />
-        </ProCard>
-      </PageContainer>
-    </ProLayout>
+        )}
+      >
+        <Outlet />
+      </ProLayout>
+    </div>
   )
 }
