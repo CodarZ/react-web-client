5cf27ae2d2b6af901a4b3b9d10749d2728eb9185
CodarZ <1476712061@qq.com>
2025-07-21 14:05:39 +0800

    fix: add type

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -10,16 +10,27 @@ interface UserInfo {
   roles?: string[]
 }
 
-export const useUserStore = create()(
+// 用户状态接口
+interface UserState {
+  userInfo: UserInfo | null
+  token: string | null
+  isLoggedIn: boolean
+  // 操作方法
+  setToken: (token: string) => void
+  setUserInfo: (userInfo: UserInfo) => void
+  clearUser: () => void
+}
+
+export const useUserStore = create<UserState>()(
   persist(
     (set) => ({
       userInfo: null,
       token: null,
       isLoggedIn: false,
 
-      setToken: (token: string) => set({ token, isLoggedIn: !!token }),
+      setToken: (token) => set({ token, isLoggedIn: !!token }),
 
-      setUserInfo: (userInfo: UserInfo) => set({ userInfo, isLoggedIn: true }),
+      setUserInfo: (userInfo) => set({ userInfo, isLoggedIn: true }),
 
       clearUser: () => {
         // 清除用户信息时，同时清除动态路由
