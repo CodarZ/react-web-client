dda3d152f0358ecfde8950055f2aaf4227df42e3
CodarZ <1476712061@qq.com>
2026-01-14 16:34:17 +0800

    feat(header): 新增退出登录

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -1,13 +1,64 @@
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import { Button, Flex, Layout, theme } from 'antd';
+import { useNavigate } from '@tanstack/react-router';
+
+import { LogoutOutlined, MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Dropdown, Flex, Layout, theme, Typography, type MenuProps } from 'antd';
+
+import { Route as LoginRoute } from '@/routes/(auth)/login';
+
+import { message, modal } from '@/libs/antd-static';
 
 const { Header: AntHeader } = Layout;
+const { Text } = Typography;
 
 /** 头部导航栏组件 */
 export function Header(props: { collapsed: boolean; onToggleCollapse: () => void }) {
   const { collapsed, onToggleCollapse } = props;
 
   const { token } = theme.useToken();
+  const navigate = useNavigate();
+
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <Flex vertical={true} style={{ cursor: 'default' }} onClick={(e) => e.preventDefault()}>
+          <Text strong={true}>超级管理员</Text>
+          <Text style={{ width: 130 }} type="secondary" ellipsis={true}>
+            admin@react-web-client
+          </Text>
+        </Flex>
+      ),
+      disabled: true,
+    },
+    { type: 'divider' },
+    { key: '2', label: '个人信息', onClick: () => message.warning('正在开发中') },
+    { key: '3', label: '账户管理', onClick: () => message.warning('正在开发中') },
+    { type: 'divider' },
+    { key: '4', label: '退出登录', danger: true, icon: <LogoutOutlined />, onClick: onLogout },
+  ];
+
+  async function onLogout() {
+    modal.confirm({
+      title: '退出登录',
+      content: '您确定要退出登录吗？您需要重新登录才能访问您的帐户。',
+      centered: true,
+      maskClosable: true,
+      onOk: async () => {
+        const hide = message.loading('正在退出...', 0);
+
+        try {
+          // 发送请求、清理状态、清理缓存
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          hide();
+          message.success('退出成功');
+          navigate({ to: LoginRoute.to, replace: true });
+        } catch {
+          hide();
+          message.error('退出失败，请稍后重试！');
+        }
+      },
+    });
+  }
 
   return (
     <AntHeader
@@ -35,7 +86,20 @@ export function Header(props: { collapsed: boolean; onToggleCollapse: () => void
           }}
         />
 
-        <Flex></Flex>
+        <Flex gap={token.padding} style={{ paddingRight: token.padding }}>
+          <Dropdown
+            menu={{ items }}
+            placement="bottomRight"
+            trigger={['click']}
+            styles={{
+              root: {
+                width: 160,
+              },
+            }}
+          >
+            <Avatar size={32} icon={<UserOutlined />} style={{ cursor: 'pointer' }} />
+          </Dropdown>
+        </Flex>
       </Flex>
     </AntHeader>
   );
