862fde44f69d1f9cc5ee4271ec463ddc0fe90c6a
CodarZ <1476712061@qq.com>
2026-01-20 17:01:44 +0800

    feat: 新增路由守卫

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { z } from 'zod';
+
 import LoginPage from '@/views/auth/login';
 
 export const Route = createFileRoute('/(auth)/login')({
+  validateSearch: z.object({
+    redirect: z.string().optional(),
+  }),
   component: LoginPage,
 });
--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,7 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 import { BaseLayout } from '@/layouts';
 
+import { useUserStore } from '@/stores/useUserStore';
+
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: ({ location }) => {
+    const isLogged = !!useUserStore.getState().token;
+    if (!isLogged) {
+      throw redirect({
+        to: '/login',
+        search: { redirect: location.href },
+      });
+    }
+  },
   component: BaseLayout,
 });
